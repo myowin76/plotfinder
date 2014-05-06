74f08a1581a5533a583a3fc15b393a6762443329
myowin76 <myowin76@gmail.com>
2014-05-06 16:07:41 +0100

    longi and lat added to plot

--- app/models/admin/plot.rb
+++ app/models/admin/plot.rb
@@ -2,4 +2,18 @@ class Admin::Plot < ActiveRecord::Base
   belongs_to :feature, :class_name => "Admin::Feature"
   belongs_to :property_type, :class_name => "Admin::PropertyType"
   belongs_to :region, :class_name => "Admin::Region"
+
+
+  geocoded_by :full_address
+  after_validation :geocode, :if => :address_changed?
+
+
+  def region_name(id)
+    Admin::Region.find_by_id(id).name unless id.nil?
+  end
+
+  def full_address
+    [address1, address2 town, postcode, region_name(region_id)].compact.join(', ')
+    # TO DO
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140506142851) do
+ActiveRecord::Schema.define(version: 20140506145436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20140506142851) do
     t.string   "postcode"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "plots", ["feature_id"], name: "index_plots_on_feature_id", using: :btree
